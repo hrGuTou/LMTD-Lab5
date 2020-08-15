2733cde40e9cf3b80b1948fa14d8852665aa1386
Haoran He <hrgutou@gmail.com>
2020-08-15 15:08:02 -0400

    Added driver code and AWS manager

--- AWSManager.py
+++ AWSManager.py
@@ -1,2 +1,25 @@
 import boto3
+import uuid
+import glob
 
+
+class AWSManager:
+    def __init__(self):
+        self.s3 = boto3.resource('s3')
+
+    def bucketGen(self, name):
+        return ''.join([name, str(uuid.uuid4())])
+
+    def listBucket(self):
+        for bucket in self.s3.buckets.all():
+            print(bucket.name)
+
+    def upload(self, bucketName, fileName):
+        try:
+            with open(fileName, 'r') as f:
+                data = f.read()
+                self.s3.Bucket(bucketName).put_object(Key=fileName, Body=data)
+        except Exception as e:
+            print(e)
+        else:
+            print("File uploaded")
--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 
 from HtmlManager import HtmlManager
+from AWSManager import AWSManager
+import glob
 
+
+def createHTML():
+        html = HtmlManager(input("Input the title for html file: "))
+
+        while True:
+            choice = input("Enter 1 to add tag, q to save: ")
+
+            if choice == "1":
+                tag, word = input("Input tag and word follows by a space")
+                html.addTag(tag, word)
+            elif choice == "q":
+                html.saveHTML()
+                break
+            else:
+                print("Invalid input")
+
+
+def upload():
+        s3 = AWSManager()
+        s3.listBucket()
+        bucketName = input("Enter your bucket name, press 'g' to create a new one: ")
+
+        if bucketName == "g":
+            bucketName = s3.bucketGen(input("Enter your name: "))
+
+        files = glob.glob("*.html")
+        for i, file in enumerate(files):
+            print(i, " ", file)
+
+        choice = input("Which file? Enter the number: ")
+
+        s3.upload(bucketName=bucketName, fileName=files[choice])
 
 
 if __name__ == "__main__":
-    title = input("Input the title for html file: ")
-    app = HtmlManager(title)
-    app.addTag("h1","Hello World")
-    app.addTag("h3", "This is a demo")
-    app.saveHTML()
+
+    createHTML()
+    upload()
